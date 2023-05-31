a999ba634af418629d8a528e152af19e62fc23df
Govindarajan Nikhilesh <nikhilesh.govind@gmail.com>
2023-05-31 11:11:35 +0800

    Initial Commit

---
+++ Github Tester/Github_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TesterApp.swift
+//  Github Tester
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
